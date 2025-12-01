498de5ddc56376f5b60f823ae1722fdd430955e3
vetacode <vetacode@gmail.com>
2025-12-02 05:50:44 +0700

    adding import expression

---
+++ dynamicImport.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// The import() expression
+let modulePath = prompt("Which module to load?");
+
+import(modulePath)
+  .then(obj => <module object>)
+  .catch(err => <loading error, e.g. if no such module>)
+
+  
