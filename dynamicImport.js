9b876fc1e1602a66afbbeb5d88af9b2710411f22
vetacode <vetacode@gmail.com>
2025-12-02 05:55:43 +0700

    adding import inside async

--- dynamicImport.js
+++ dynamicImport.js
@@ -1,10 +1,37 @@
-'use strict'
+'use strict';
 
 // The import() expression
-let modulePath = prompt("Which module to load?");
+let modulePath = 'Which module to load?';
 
-import(modulePath)
-  .then(obj => <module object>)
-  .catch(err => <loading error, e.g. if no such module>)
+import(modulePath);
+// .then(obj => <module object>);
+// .catch(err => <loading error, e.g. if no such module>)
 
-  
+{
+  //Inside async
+  // 📁 say.js
+  // export function hi() {
+  //   alert(`Hello`);
+  // }
+
+  // export function bye() {
+  //   alert(`Bye`);
+  // }
+
+  let { hi, bye } = await import('./say.js');
+
+  hi();
+  bye();
+}
+
+{
+  //If export use default:
+  // 📁 say.js
+  // export default function() {
+  //   alert("Module loaded (export default)!");}
+  let obj = await import('./say.js');
+  let say = obj.default;
+  // or, in one line: let {default: say} = await import('./say.js');
+
+  say();
+}
